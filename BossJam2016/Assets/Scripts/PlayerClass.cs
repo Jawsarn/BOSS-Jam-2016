da137871b0523d8c5a5fd393c5a469d64078c70f
Jawsarn <christian_markowicz@hotmail.com>
2016-04-16 02:36:39 +0200

    Added replication to clients from classes

--- BossJam2016/Assets/Scripts/PlayerClass.cs
+++ BossJam2016/Assets/Scripts/PlayerClass.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 
-public class PlayerClass : MonoBehaviour {
+public class PlayerClass : NetworkBehaviour {
 
     // 0 is none, 1 is mainship, 2 is turret, 3 is chaser
     public int classType = 0;
@@ -28,7 +29,9 @@ public class PlayerClass : MonoBehaviour {
             case 1:
                 GameObject ship = (GameObject)Instantiate(BigShipPrefab, Vector3.zero, Quaternion.Euler(Vector3.zero));
                 ship.transform.parent = transform;
-                
+
+                NetworkServer.Spawn(ship);
+
                 break;
 
             // turret
@@ -43,6 +46,8 @@ public class PlayerClass : MonoBehaviour {
                     turret.transform.parent = transform;
 
                     transform.parent = bigship.transform;
+
+                    NetworkServer.Spawn(turret);
                 }
 
 
@@ -53,9 +58,66 @@ public class PlayerClass : MonoBehaviour {
                 GameObject chaser = (GameObject)Instantiate(ChaserPrefab, Vector3.zero, Quaternion.Euler(Vector3.zero));
                 chaser.transform.parent = transform;
 
+                NetworkServer.Spawn(chaser);
+
                 break;
             default:
                 break;
         }
+        // Give client an ID
+        RpcSetClassID(classType);
+    }
+
+    [ClientRpc]
+    void RpcSetClassID(int inClassType)
+    {
+        classType = inClassType;
+
+        switch (classType)
+        {
+            // mainship
+            case 1:
+                GameObject[] ships = GameObject.FindGameObjectsWithTag("BigShip");
+                if (ships.GetLength(0) > 0)
+                {
+                    ships[0].transform.parent = transform;
+                }
+
+                break;
+
+            // turret
+            case 2:
+                // We assume there is a ship now..
+                GameObject[] gameobjects = GameObject.FindGameObjectsWithTag("BigShip");
+
+                if (gameobjects.GetLength(0) == 1)
+                {
+                    GameObject bigship = gameobjects[0];
+                    GameObject[] turrets = GameObject.FindGameObjectsWithTag("Turret");
+                    if (turrets.GetLength(0) > 0)
+                    {
+                        turrets[0].transform.parent = transform;
+                    }
+
+                    transform.parent = bigship.transform;
+                }
+
+
+                break;
+
+            //chaser
+            case 3:
+                GameObject[] chasers = GameObject.FindGameObjectsWithTag("Chaser");
+                if (chasers.GetLength(0) > 0)
+                {
+                    chasers[0].transform.parent = transform;
+                }
+
+                break;
+            default:
+                break;
+        }
+        PlayerManager playerMan = GetComponent<PlayerManager>();
+        
     }
 }
