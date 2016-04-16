7d638efeb4be9e4ed153c6d7febf06ff6fe1005c
Jawsarn <christian_markowicz@hotmail.com>
2016-04-16 13:39:39 +0200

    Added gunslot and guntype scripts

---
+++ BossJam2016/Assets/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GunController : MonoBehaviour {
+
+    List<GameObject> guns = new List<GameObject>(); 
+
+	// Use this for initialization
+	void Start () {
+        foreach (Transform item in transform)
+        {
+            if (item.GetComponent<GunType>() != null)
+            {
+                guns.Add(item.gameObject);
+            }
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
