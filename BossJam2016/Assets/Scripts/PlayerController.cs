f37c2b73d9475278cc6d4ad2a02c4aa39005111a
Ali Hassan <alihassan_80@hotmail.com>
2016-04-16 11:07:05 +0200

    Camera and PlayerController

---
+++ BossJam2016/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector3 position;
+    public Vector3 velocity;
+
+    public float startingVelocityZ = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        position = new Vector3(0, 0, 0);
+        velocity = new Vector3(0, 0, startingVelocityZ);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position += velocity;
+	}
+}
